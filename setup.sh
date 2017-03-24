7b159de9dc4830b1ff24f963ba0df4bcdb18ec4c
Ben Donnaloia <ben.donnaloia@gmail.com>
2017-03-23 17:18:58 -0700

    added hostnames to pg_hba.conf

--- setup.sh
+++ setup.sh
@@ -7,7 +7,8 @@ CONF="/etc/postgresql/9.5/main/postgresql.conf"
 # Restrict subnet to docker private network
 echo "host    all             all             172.17.0.0/16               md5" >> /etc/postgresql/9.5/main/pg_hba.conf
 # Allow access from locationserver
-echo "host    all             all             cannablrv2-locationserver.test.kontena.local               md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+echo "host    all             all             cannablrv2-locationserver-1.test.kontena.local               md5" >> /etc/postgresql/9.5/main/pg_hba.conf
+echo "host    all             all             cannablrv2-parentserver-1.test.kontena.local               md5" >> /etc/postgresql/9.5/main/pg_hba.conf
 # And allow access from DockerToolbox / Boottodocker on OSX
 echo "host    all             all             192.168.0.0/16               md5" >> /etc/postgresql/9.5/main/pg_hba.conf
 # Listen on all ip addresses
